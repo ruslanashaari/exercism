847fa0f723b5957eb7a8bed82286d20a0a40027f
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-17 19:11:48 +0800

    implementation using array instead of loop

--- php/difference-of-squares/difference-of-squares.php
+++ php/difference-of-squares/difference-of-squares.php
@@ -1,19 +1,15 @@
 <?php
 
-function squareOfSums($number,  $sum = 0) {
-	for ($x = 1; $x <= $number; $x++) {
-		$sum += $x;
-	}
-
-	return pow($sum, 2);
+function arrayX2($x) {
+	return pow($x, 2);
 }
 
-function sumOfSquares($number, $sum = 0) {
-	for ($x = 1; $x <= $number; $x++) {
-		$sum += pow($x, 2);
-	}
+function squareOfSums($number) {
+	return pow(array_sum(range(1, $number)), 2);
+}
 
-	return $sum;
+function sumOfSquares($number, $x = 1) {
+	return array_sum(array_map("arrayX2", range(1, $number)));
 }
 
 function difference($number) {
