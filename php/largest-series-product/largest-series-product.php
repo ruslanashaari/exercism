f5730a9e71f95d875c28956fb050cc770636b2ec
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-18 22:28:07 +0800

    add some loop and constructor; still cant figure out how to solve this

--- php/largest-series-product/largest-series-product.php
+++ php/largest-series-product/largest-series-product.php
@@ -1,2 +1,26 @@
 <?php
 
+class Series {
+
+	protected $series;
+	
+	public function __construct($series) {
+		$this->series = $series;
+	}
+
+	function largestProduct($number_count, $product = 0, $temp = 0, $x = 0, $y = 0) {
+		$arr_series = str_split($this->series);
+
+		for ($i=0; $i<=sizeof($arr_series); $i++) { 
+			$temp = $arr_series[$i] * $arr_series[$i+1];
+
+			if ($temp > $product){
+				$x = $arr_series[$i];
+				$y = $arr_series[$i+1];
+			}
+		}
+
+		return $y.$x;
+	}
+}
+
