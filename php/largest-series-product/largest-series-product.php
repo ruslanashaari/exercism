900fbbc4eb9bada15411386c084495d495b49f70
ruslanashaari <ruslan.ashaarigmail.com>
2017-09-18 22:53:44 +0800

    try addin another loop for number count

--- php/largest-series-product/largest-series-product.php
+++ php/largest-series-product/largest-series-product.php
@@ -8,11 +8,16 @@ class Series {
 		$this->series = $series;
 	}
 
-	function largestProduct($number_count, $product = 0, $temp = 0, $x = 0, $y = 0) {
+	function largestProduct($number_count, $product = 0, $temp = 1, $x = 0, $y = 0) {
 		$arr_series = str_split($this->series);
+		// $limit = sizeof($arr_series)-1;
 
-		for ($i=0; $i<=sizeof($arr_series); $i++) { 
-			$temp = $arr_series[$i] * $arr_series[$i+1];
+		for ($i=0; $i<sizeof($arr_series)-1; $i++) { 
+
+			for ($x=0; $x<=$number_count-1; $x++) { 
+				$temp *= $arr_series[$i+$x];
+			}
+			// $temp = $arr_series[$i] * $arr_series[$i+1];
 
 			if ($temp > $product){
 				$x = $arr_series[$i];
