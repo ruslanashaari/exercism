4eb80c3bd7f29f445dd6ffca72baf767418926fa
ruslanashaari <ruslan.ashaari@gmail.com>
2020-01-25 09:11:20 +0800

    fix hamming code to pass the equal length exception

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -4,6 +4,7 @@
 // This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+// distance('GGACG', 'AGGACGTGG');
 
 function distance($a, $b)
 {
@@ -13,6 +14,10 @@ function distance($a, $b)
     $ax = str_split($a);
     $bx = str_split($b);
     $dist = 0;
+
+    if (count($ax) !== count($bx)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
 
     for($i = 0; $i<count($ax); $i++){
     	if($ax[$i] != $bx[$i])
