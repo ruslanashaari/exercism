7ddf1bfe252f4d77247cc0dd1e32556544630b8e
ruslanashaari <ruslan.ashaari@gmail.com>
2020-01-27 18:11:25 +0800

    improve by implementing class

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -1,17 +1,40 @@
 <?php
 
-class Bob{
-	function respondTo($a){
-  
-		if (strtoupper($a) == $a && preg_match('/[A-Z]/', $a)) {
+class Bob {
+	function respondTo($statement){
+
+		if (Statement::isYelling($statement)) {
 			return "Whoa, chill out!";
-		} elseif (preg_match('/[?]$/', trim($a))) {
-			return "Sure.";
-		} elseif (strlen(trim($a)) == 0) {
-			return "Fine. Be that way!";
-		} else {
-			return "Whatever.";	
 		}
+
+		if (Statement::isQuestion($statement)) {
+			return "Sure.";
+		}
+
+		if (Statement::isNothing($statement)) {
+			return "Fine. Be that way!";
+		}
+
+		return "Whatever.";			
 	}
+}
+
+class Statement {
+	
+	static function isYelling($statement)
+	{
+		return (strtoupper($a) == $a && preg_match('/[A-Z]/', $a);
+	}
+
+	static function isQuestion($statement)
+	{
+		return (preg_match('/[?]$/', trim($a)));
+	}
+
+	static function isNothing($statement)
+	{
+		return (strlen(trim($a)) == 0);
+	}
+
 }
 ?>
